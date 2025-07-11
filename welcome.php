435b178e3578648fb05a8d1a2a1ab87f186b1141
Donthireddysivakumari <167413914+Donthireddysivakumari@users.noreply.github.com>
2025-07-11 21:45:39 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body bgcolor="pink">
+<h1>Welcome to Online Train Reservation System</h1>
+<h1><a class="button" href="index.php">Register</a><br></h1>
+<h1><a class="button" href="LOGIN.php">Login</a><br><br></h1>
+</body>
+</html>
